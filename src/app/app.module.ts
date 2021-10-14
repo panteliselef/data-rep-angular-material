fd1e06dbd4132f0303dac86c1c87b32af7a2e927
elefcodes <panteliselef@outlook.com>
2021-10-14 14:14:50 +0300

    Removing legacy components that are broken

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,13 +31,9 @@ import {VisModule} from 'ngx-vis';
 
 import {HeaderComponent} from './components/header/header.component';
 import {DiseaseNetworkComponent} from './components/disease-network/disease-network.component';
-import {VisualizationsComponent} from './components/visualizations/visualizations.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {HomepageComponent} from './components/homepage/homepage.component';
 import {ToolbarSearchComponent} from './components/toolbar-search/toolbar-search.component';
-import {DiseasePageComponent} from './components/disease-page/disease-page.component';
-import {NodeDetailsComponent} from './components/disease-page/node-details/node-details.component';
-import {EdgeDetailsComponent} from './components/disease-page/edge-details/edge-details.component';
 import {DatasetNetworkComponent} from './components/dataset-network/dataset-network.component';
 import {ToolbarSearchDatasetComponent} from './components/toolbar-search-dataset/toolbar-search-dataset.component';
 import {PhenonetNetworkComponent} from './components/v2/phenonet-network-page/phenonet-network.component';
@@ -65,13 +61,9 @@ export function importingEchart(): void {
     HeaderComponent,
     HomepageComponent,
     DiseaseNetworkComponent,
-    VisualizationsComponent,
     NavbarComponent,
     NavbarComponent,
     ToolbarSearchComponent,
-    DiseasePageComponent,
-    NodeDetailsComponent,
-    EdgeDetailsComponent,
     DatasetNetworkComponent,
     ToolbarSearchDatasetComponent,
     PhenonetNetworkComponent,
