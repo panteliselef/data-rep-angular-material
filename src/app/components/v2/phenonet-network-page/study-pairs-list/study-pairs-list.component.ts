cf370c15ce913771caacb85280f1954a2eae2f02
pantelis.eleftheriadis <pantelis.eleftheriadis@optima-apps.com>
2022-04-28 19:33:58 +0200

    95 navigating to proper route after making request for getting the platform in which the selected edge exists

--- src/app/components/v2/phenonet-network-page/study-pairs-list/study-pairs-list.component.ts
+++ src/app/components/v2/phenonet-network-page/study-pairs-list/study-pairs-list.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {DATASET_PAIR} from 'src/app/models/graph.model';
+import {ApiService} from 'src/app/services/api.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-study-pairs-list',
@@ -13,8 +15,25 @@ export class StudyPairsListComponent implements OnInit {
   @Input() primaryDisease: string;
   @Input() secondaryDisease: string;
 
-  constructor() { }
+  constructor(private apiService: ApiService, private router: Router) {
+  }
 
   ngOnInit(): void {
+  }
+
+  navigateToPlatform(studyPair: DATASET_PAIR): void {
+    console.log(studyPair);
+    this.apiService.getPlatformOfEdge(studyPair.dA, studyPair.dB)
+      .subscribe(({platform, error}) => {
+        if (error) {
+          return;
+        }
+
+        this.router.navigate(['v3', 'platforms', platform, studyPair.dA], {
+          queryParams: {
+            edgeWith: studyPair.dB
+          }
+        });
+      });
   }
 }
